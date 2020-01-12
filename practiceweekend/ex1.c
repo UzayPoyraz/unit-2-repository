d837cea4e9996a213d8513577a6ab1244413bf14
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-01-12 22:33:02 +0900

    Update ex1.c

--- practiceweekend/ex1.c
+++ practiceweekend/ex1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+//this program gets the sum of 10 numbers entered
+int main(void) {
+  int num, sum;
+  for(int i=0; i<10; i++){
+    printf("Enter 10 numbers\n");
+    scanf("%d", &num);
+    sum += num;
+printf("The sum of 10 numbers is %d", sum);
+return 0;
 
+
+  }
+
+
+
+}
