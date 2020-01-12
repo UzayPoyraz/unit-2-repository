512cd1f7eed5860c98e2d8a3c3c04feb10ce37b3
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-01-13 08:28:24 +0900

    Create ex6.c

---
+++ practiceweekend/ex6.c
@@ -0,0 +1,19 @@
+// THIS PROGRAM FINDS PERFECT NUMBERS IN AN ENTERED RANGE
+
+#include <stdio.h>
+
+int main(void) {
+int begin;
+int end;
+
+printf(Starting number of the range\n");
+  scanf("%d", &begin);
+  printf("Ending number of the range.\n");
+  scanf("%d", &end);
+  for (int i=1; i<=end; i++) {
+
+
+  return 0;
+}
+
+I can't go further ://
