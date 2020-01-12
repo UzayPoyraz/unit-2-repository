63a7337db528856cc0a5d092421d8f851c5b4d2b
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-01-13 08:41:53 +0900

    Create ex7.c

---
+++ practiceweekend/ex7.c
@@ -0,0 +1,18 @@
+I am struggling on this one too!
+// This program makes a pascal triangle out of the number entered
+
+#include <stdio.h>
+
+int main(void) {
+int sum;
+int term;
+
+printf("Enter what term you want the Pascal Tirangle to get to");
+scanf("%d", &term);
+for(int i=0; i<=term; i ++){
+  
+}
+
+
+  return 0;
+}
