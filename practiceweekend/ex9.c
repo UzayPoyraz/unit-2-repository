1fac055c6ef83ecd9ea2639591a5eb579b4468d2
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2020-01-13 09:10:06 +0900

    Create ex9.c

---
+++ practiceweekend/ex9.c
@@ -0,0 +1,17 @@
+I'm stuck ater the for loop
+// This program reverses a string entered
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+   char string[100], rev;
+   int length;
+printf("Enter a string");
+scanf("%s", string);
+length=strlen(string);
+for(int i=0; i<=length; i++);
+
+
+   
+}
